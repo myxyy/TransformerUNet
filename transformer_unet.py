9ae97b9224a8a2c98d148e18b4d9d39d9aa68266
myxyy <m.myxomatosis@gmail.com>
2023-02-25 22:35:17 +0900

    SparseMHAEncoderを一応実装、一応動く

--- transformer_unet.py
+++ transformer_unet.py
@@ -1,4 +1,5 @@
 from transformer import TransformerDecoder, TransformerEncoder
+from transformer import SparseSelfTransformer
 from positional_encoding import PositionalEncoding
 from mask import SelfMask, CrossEncodeMask, CrossDecodeMask
 
@@ -61,6 +62,64 @@ class TransformerUNetSequence(nn.Module):
         if self.enable_post:
             self_encoder_post = self.self_encoder_post_list[depth]
             x = self_encoder_post(x, self_mask)
+        return x
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.unet_rec(self.positional_encoding_list[0](x), 0)
+       
+class SparseTransformerUNetSequence(nn.Module):
+    def __init__(self, length: int, downsample_rate: float, depth_unet: int, depth_transformer: int, dim: int, dim_scale: float, head_num: int, dropout: int, enable_pre=True, enable_middle=True, enable_post=True):
+        super().__init__()
+        self.enable_pre = enable_pre
+        self.enable_middle = enable_middle
+        self.enable_post = enable_post
+        depth_unet = min(depth_unet, -(int)(math.log(length, downsample_rate)))
+        self.depth_unet = depth_unet
+        self.dim = dim
+        self.dim_scale = dim_scale
+        self.encoder_list = nn.ModuleList([TransformerDecoder(self.level_i_dim(i+1), self.level_i_dim(i), head_num, depth_transformer, dropout) for i in range(depth_unet)])
+        self.decoder_list = nn.ModuleList([TransformerDecoder(self.level_i_dim(i), self.level_i_dim(i+1), head_num, depth_transformer, dropout) for i in range(depth_unet)])
+        if enable_pre:
+            self.self_encoder_pre_list = nn.ModuleList([SparseSelfTransformer(self.level_i_dim(i), head_num, depth_transformer, dropout, 4, 1) for i in range(depth_unet+1)])
+        if enable_middle:
+            self.self_encoder_middle_list = nn.ModuleList([SparseSelfTransformer(self.level_i_dim(i), head_num, depth_transformer, dropout, 4, 1) for i in range(depth_unet+1)])
+        if enable_post:
+            self.self_encoder_post_list = nn.ModuleList([SparseSelfTransformer(self.level_i_dim(i), head_num, depth_transformer, dropout, 4, 1) for i in range(depth_unet+1)])
+        self.encoder_cross_mask_list = nn.ModuleList([CrossEncodeMask((int)(length*(downsample_rate**i)),(int)(length*(downsample_rate**(i+1)))) for i in range(depth_unet)])
+        self.decoder_cross_mask_list = nn.ModuleList([CrossDecodeMask((int)(length*(downsample_rate**(i+1))),(int)(length*(downsample_rate**i))) for i in range(depth_unet)])
+        self.positional_encoding_list = nn.ModuleList([PositionalEncoding((int)(length*(downsample_rate**i)), self.level_i_dim(i)) for i in range(depth_unet+1)])
+
+    def level_i_dim(self, i):
+        return (int)(math.ceil(self.dim*(self.dim_scale**i)/2)*2)
+
+    # (batch, length, dim) -> (batch, length, dim)
+    def unet_rec(self, x: torch.Tensor, depth: int) -> torch.Tensor:
+        if depth < self.depth_unet:
+            batch = x.shape[0]
+            encoder = self.encoder_list[depth]
+            decoder = self.decoder_list[depth]
+            encoder_cross_mask = self.encoder_cross_mask_list[depth]
+            decoder_cross_mask = self.decoder_cross_mask_list[depth]
+            positional_encoding = self.positional_encoding_list[depth+1]().repeat(batch, 1, 1)
+
+        if self.enable_pre:
+            self_encoder_pre = self.self_encoder_pre_list[depth]
+            x = self_encoder_pre(x)
+
+        if depth < self.depth_unet:
+            y = encoder(positional_encoding, x, encoder_cross_mask)
+            y = self.unet_rec(y, depth + 1)
+
+        if self.enable_middle:
+            self_encoder_middle = self.self_encoder_middle_list[depth]
+            x = self_encoder_middle(x)
+
+        if depth < self.depth_unet:
+            x = decoder(x, y, decoder_cross_mask)
+
+        if self.enable_post:
+            self_encoder_post = self.self_encoder_post_list[depth]
+            x = self_encoder_post(x)
         return x
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
