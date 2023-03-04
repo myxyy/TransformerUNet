ecce8cb92863d18acffe3f3120d32bcc429ff0fe
myxyy <m.myxomatosis@gmail.com>
2023-03-04 12:07:48 +0900

    Encoder部分実装

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ class GPTUNet(pl.LightningModule):
         return optimizer
 
 model = GPTUNet(
-    TransformerUNetSequence,
+    SparseTransformerUNetSequenceOptimized,
     length=1024,
     downsample_rate=0.5,
     depth_unet=10,
--- transformer_sparse_optimized.py
+++ transformer_sparse_optimized.py
@@ -12,17 +12,33 @@ class SparseMHAEncoder(nn.Module):
         self.kK = nn.Linear(dim_k, head_num * dim_QK, bias=False)
         self.vV = nn.Linear(dim_v, head_num * dim_V, bias=False)
         self.linear_out = nn.Linear(head_num * dim_V, dim_out, bias=False)
-        self.softmax = nn.Softmax(dim=3)
         self.span = span
         self.stride = stride
         self.length_q = length_q
         self.length_kv = length_kv
-        self.row, self.column = torch.meshgrid(torch.arange(self.span), torch.arange(length_q), indexing='ij')
-        self.kvi = self.row - (self.span-1) + torch.arange(0,self.stride*length_q,self.stride) # key-value index
-        self.is_valid_kvi = torch.logical_and(self.kvi >= 0, self.kvi < length_kv)
-        self.vkvi_row, self.vkvi_column = torch.where(self.is_valid_kvi) # valid key-value index
-        self.ivkvi_row, self.ivkvi_column = torch.where(~self.is_valid_kvi) # invalid key-value index
 
+        row, column = torch.meshgrid(torch.arange(span), torch.arange(length_q), indexing='ij')
+        kvi = row - (span-1) + torch.arange(0,stride*length_q,stride) # key-value index
+        is_valid_kvi = torch.logical_and(kvi >= 0, kvi < length_kv)
+        ivkvi_row, ivkvi_column = torch.where(~is_valid_kvi) # invalid key-value index
+
+        conv_K_weight_ind_key, conv_K_weight_ind_span = torch.meshgrid(torch.arange(dim_QK), torch.arange(span), indexing='ij')
+        conv_K_weight = torch.zeros(dim_QK, span, dim_QK, span)
+        conv_K_weight[conv_K_weight_ind_key, conv_K_weight_ind_span, conv_K_weight_ind_key, conv_K_weight_ind_span] = 1
+        conv_K_weight = conv_K_weight.reshape(dim_QK*span, dim_QK, span)
+        self.conv_K = nn.Conv1d(dim_QK, dim_QK*span, span, stride, padding=span-1)
+        self.conv_K.weight.data = conv_K_weight
+        self.conv_K.weight.requires_grad = False
+        self.table_QK_mask = torch.zeros(span, length_q).cuda()
+        self.table_QK_mask[ivkvi_row, ivkvi_column] = -float('inf')
+        conv_V_weight_ind_key, conv_V_weight_ind_span = torch.meshgrid(torch.arange(dim_QK), torch.arange(span), indexing='ij')
+        conv_V_weight = torch.zeros(dim_V, span, dim_V, span)
+        conv_V_weight[conv_V_weight_ind_key, conv_V_weight_ind_span, conv_V_weight_ind_key, conv_V_weight_ind_span] = 1
+        conv_V_weight = conv_V_weight.reshape(dim_V*span, dim_V, span)
+        self.conv_V = nn.Conv1d(dim_V, dim_V*span, span, stride, padding=span-1)
+        self.conv_V.weight.data = conv_V_weight
+        self.conv_V.weight.requires_grad = False
+ 
     # (batch, length_q, dim_q)
     # (batch, length_kv, dim_k)
     # (batch, length_kv, dim_v)
@@ -34,23 +50,25 @@ class SparseMHAEncoder(nn.Module):
         K = self.kK(k) # (batch, length_kv, head_num * dim_QK)
         V = self.vV(v) # (batch, length_kv, head_num * dim_V)
         
-        Q = Q.view(batch, self.length_q, self.head_num, self.dim_QK).permute(0,2,1,3) # (batch, head_num, length_q, dim_QK)
-        K = K.view(batch, self.length_kv, self.head_num, self.dim_QK).permute(0,2,1,3) # (batch, head_num, length_kv, dim_QK)
-        V = V.view(batch, self.length_kv, self.head_num, self.dim_V).permute(0,2,1,3) # (batch, head_num, length_kv, dim_V)
+        bh = batch * self.head_num
+        Q = Q.view(batch, self.length_q, self.head_num, self.dim_QK).permute(0,2,3,1).reshape(bh, self.dim_QK, self.length_q)
+        K = K.view(batch, self.length_kv, self.head_num, self.dim_QK).permute(0,2,3,1).reshape(bh, self.dim_QK, self.length_kv)
+        V = V.view(batch, self.length_kv, self.head_num, self.dim_V).permute(0,2,3,1).reshape(bh, self.dim_V, self.length_kv)
 
-        # print(f"Q.shape:{Q.shape}")
-        # print(f"K.shape:{K.shape}")
-        # print(f"V.shape:{V.shape}")
-        QK_table = torch.empty((batch, self.head_num, self.span, self.length_q), device='cuda') # (batch, head_num, span, length_q)
-        QK_table[:,:,self.vkvi_row,self.vkvi_column] = torch.matmul(Q[:,:,self.column[self.vkvi_row,self.vkvi_column],:].unsqueeze(3), K[:,:,self.kvi[self.vkvi_row,self.vkvi_column],:].unsqueeze(4)).reshape(batch,self.head_num,len(self.vkvi_row))/(self.dim_QK ** 0.5)
-        QK_table[:,:,self.ivkvi_row,self.ivkvi_column] = -float('inf')
-        QKs_table = QK_table.softmax(2) # (batch, head_num, span, length_q)
-        QKs_table_V = QKs_table.unsqueeze(4).repeat(1,1,1,1,self.dim_V) # (batch, head_num, span, length_q, dim_V)
-        QKs_table_V[:,:,self.vkvi_row, self.vkvi_column,:] *= V[:,:,self.kvi[self.vkvi_row, self.vkvi_column],:]
-        QKV_table = QKs_table_V
-        QKV = QKV_table.sum(2).permute(0,2,1,3).reshape(batch, self.length_q, self.head_num * self.dim_V)
-        out = self.linear_out(QKV)
-        #print(f'length_q:{length_q}, length_kv:{length_kv}, len(vkvi_row):{len(vkvi_row)}, len(ivkvi_row):{len(ivkvi_row)}')
+        table_K = self.conv_K(K) # (bh, dim_QK*span, ?)
+        table_K = table_K[:,:,0:self.length_q]
+        table_K = table_K.reshape(bh, self.dim_QK, self.span, self.length_q).permute(0,2,3,1) # (bh, span, length_q, dim_QK)
+        Q = Q.expand(self.span,bh,self.dim_QK,self.length_q) # (span, bh, dim_QK, length_q)
+        Q = Q.permute(1,0,3,2).unsqueeze(3)
+        table_K = table_K.unsqueeze(4)
+        table_QK = torch.matmul(Q, table_K)/(self.dim_QK ** 0.5)
+        table_QK = table_QK.reshape(bh, self.span, self.length_q)
+        table_QK = table_QK + self.table_QK_mask
+        table_QK = table_QK.softmax(1)
+        table_V = self.conv_V(V)[:,:,0:self.length_q].reshape(bh, self.dim_V, self.span, self.length_q).permute(0,2,3,1) # (bh, span, length_q, dim_V)
+        table_QKV = table_QK.unsqueeze(3) * table_V # (bh, span, length_q, dim_V)
+        out = self.linear_out(table_QKV.sum(1).reshape(batch, self.head_num, self.length_q, self.dim_V).permute(0,2,1,3).reshape(batch, self.length_q, self.head_num*self.dim_V))
+
         return out
 
 class SparseSelfTransformerBlock(nn.Module):
